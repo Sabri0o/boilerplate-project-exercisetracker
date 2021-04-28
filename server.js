d00c32d21df5248d64ff1b0353e403da4c0210d0
Sabri0o <sabritrabelsi90@gmail.com>
2021-04-28 17:58:27 +0100

    create new user

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+//The express.json() and express.urlencoded() middleware have been added to provide request body parsing support out-of-the-box.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
 // connecting database
-const mySecret = process.env['MONGO_URI']
+const mySecret = process.env["MONGO_URI"];
 
 let mongoose;
 try {
@@ -25,7 +27,7 @@ mongoose.connect(
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false 
+    useFindAndModify: false,
   },
   function (error) {
     if (error) {
@@ -35,6 +37,53 @@ mongoose.connect(
   }
 );
 
+// importing UserTracker modal
+const UserTracker = require("./dbSchema.js").UserTrackerModel;
+
+// **************** create a new user ***************
+
+// check if the username exists in db middleware
+const checkUserName = function (req, res, next) {
+  let userName = req.body.username;
+  UserTracker.findOne({ username: userName }).then((record) => {
+    if (record) {
+      res.send("Username already taken");
+    } else {
+      console.log("proceeding registration...");
+      next();
+    }
+  });
+};
+
+// create a new user
+app.post("/api/users", checkUserName, function (req, res) {
+  let userName = req.body.username;
+  var newUser = new UserTracker({
+    username: userName,
+  });
+  newUser.save(function (err, record) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("new user is saved successfully");
+      res.json({ username: record.username, _id: record._id });
+    }
+  });
+});
+
+// ***************** get an array of all users ****************
+app.get("/api/users", function (req, res) {
+  console.log("*********************");
+  var query = UserTracker.find();
+  query
+    .select(["_id", "username"])
+    .then((records) => {
+      // console.log(records)
+      res.send(records);
+    })
+    .catch((err) => res.send(err.message));
+});
+
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
